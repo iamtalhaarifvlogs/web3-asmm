ed2ca549c0e07d9a31e5582cb2dfb16d6b34e101
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-04-15 21:23:49 +0500

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { createConfig, http } from "wagmi";
+import { bsc, bscTestnet } from "wagmi/chains";
+import { injected, walletConnect } from "wagmi/connectors";
+
+const queryClient = new QueryClient();
+
+const config = createConfig({
+  chains: [bsc, bscTestnet],
+  connectors: [
+    injected(),
+    walletConnect({
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    }),
+  ],
+  transports: {
+    [bsc.id]: http(),
+    [bscTestnet.id]: http(),
+  },
+  ssr: true,
+});
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}

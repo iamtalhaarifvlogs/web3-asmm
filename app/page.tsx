c4efb1156bf9c233c989ca28f781d861f18c10f2
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-04-16 02:59:54 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ import {
 } from "wagmi";
 
 import { formatUnits, parseUnits } from "viem";
-import { erc20Abi } from "@/lib/erc20Abi";
-import { BSC_CHAIN_ID, RECEIVER_WALLET, USDT_BSC } from "@/lib/constants";
+import { erc20Abi } from "@/app/lib/erc20Abi";
+import { BSC_CHAIN_ID, RECEIVER_WALLET, USDT_BSC } from "@/app/lib/constants";
 
 export default function HomePage() {
   const { address, isConnected } = useAccount();
